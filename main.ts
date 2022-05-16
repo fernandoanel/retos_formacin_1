e61f6fc5283926141cb85810b0225e2105e4a019
Fernando Anel <anel@coitt.es>
2022-05-16 10:27:34 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,46 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+basic.showString("¡Hola Mundo!")
+basic.forever(function () {
+	
+})
